fc5c35bf0219ae8e8b754989726a230dfac22411
Robertino Mihai Sandorhazi <49100220+nightdevil00@users.noreply.github.com>
2023-10-22 18:48:22 +0300

    Update MyArch.sh

--- MyArch.sh
+++ MyArch.sh
@@ -1,7 +1,8 @@
 sudo pacman -S --needed - < packages.x86_64
 sudo systemctl enable bluetooth --now
 
-sudo git https://aur.archlinux.org/yay-bin.git
+sudo git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 sudo makepkg -si
+cd ...
 ./reinstall_aur.sh
